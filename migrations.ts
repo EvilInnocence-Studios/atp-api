912fe6338fc71d0ad3cd599be80f991d8de70d3a
Andrew Wittrock <andy@wittrock.us>
2025-11-23 23:58:35 -0600

    Update migration order

--- migrations.ts
+++ migrations.ts
@@ -1,17 +1,16 @@
 import { migrations as brokeredProductsPluginMigrations } from "./src/brokered-products-plugin/migrations";
 import { migrations as comicMigrations } from "./src/comic/migrations";
-import { migrations as commonMigrations } from "./src/common/migrations";
 import { migrations as coreMigrations } from "./src/core/migrations";
 import { migrations as storeMigrations } from "./src/store/migrations";
 import { migrations as subscriptionMigrations } from "./src/subscription/migrations";
 import { migrations as subscriptionProductsPluginMigrations } from "./src/subscription-products-plugin/migrations";
+import { IMigration } from "./src/core/dbMigrations";
 
-export const migrations = [
-    ...brokeredProductsPluginMigrations,
-    ...comicMigrations,
-    ...commonMigrations,
+export const migrations: IMigration[] = [
     ...coreMigrations,
+    ...comicMigrations,
     ...storeMigrations,
     ...subscriptionMigrations,
+    ...brokeredProductsPluginMigrations,
     ...subscriptionProductsPluginMigrations,
 ];
