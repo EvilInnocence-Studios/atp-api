1d97f9386e8e3c29b4bb494bc50840c9d632ebd2
Andrew Wittrock <andy@wittrock.us>
2026-06-19 21:59:46 -0500

    Update dependencies

--- src/generate-password-hash.ts
+++ src/generate-password-hash.ts
@@ -4,4 +4,4 @@ import { User } from "./uac/user/service";
 const newPassword = process.argv[2];
 console.log(newPassword);
 
-console.log(User.hashPassword(newPassword));
+console.log(User.legacyHashPassword(newPassword));
